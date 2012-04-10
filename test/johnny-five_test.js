a402325beaffe050eca5b0445a9fb0696f5efde1
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-04-10 16:48:56 -0400

    New tests for board hardware caching
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- test/johnny-five_test.js
+++ test/johnny-five_test.js
@@ -13,14 +13,40 @@ var SerialPort = require("./mock-serial").SerialPort,
 
 console.log( board );
 
-
-exports["board"] = {
-  "board": function( test ) {
-
-    // TODO: Figure out a way to run tests without a board connect
-
+exports["five.Board (static)"] = {
+  "Board.cache": function( test ) {
+    test.expect(2);
+    test.equal( typeof five.Board.cache, "object", "Board.cache" );
+    test.ok( Array.isArray(five.Board.cache), "Board.cache" );
+    test.done();
+  },
+  "Board.uid": function( test ) {
     test.expect(1);
-    test.ok(board, "Board instance");
+    test.equal( typeof five.Board.uid, "function", "Board.uid" );
+    test.done();
+  }
+};
+
+
+exports["five.Board (instance)"] = {
+  "cache": function( test ) {
+    test.expect(1);
+    test.equal( five.Board.cache.length, 1, "Board cached" );
+    test.done();
+  },
+  "instance": function( test ) {
+    test.expect(1);
+    test.ok( board, "Board instance" );
+    test.done();
+  },
+  "firmata": function( test ) {
+    test.expect(1);
+    test.ok( board.firmata, "Board firmata" );
+    test.done();
+  },
+  "id": function( test ) {
+    test.expect(1);
+    test.ok( board.id, "Board id" );
     test.done();
   }
 };
