57000b5bbf0b1ffea732efaf2d30ed3ad3892562
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-04-10 16:56:27 -0400

    Remove node_modules
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- node_modules/firmata/node_modules/serialport/tests/test_write.js
+++
@@ -1,8 +0,0 @@
-// Test with the epic VirtualSerialPortApp - http://code.google.com/p/macosxvirtualserialport/
-
-var SerialPort = require("../serialport").SerialPort;
-
-var serial_port = new SerialPort("/dev/master", {baudrate: 9600});
-
-serial_port.write("It worked!\r");
-serial_port.close();
