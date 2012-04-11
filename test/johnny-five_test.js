2788919f0144917854e2a49d87b49088186d4e73
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-04-11 11:30:56 -0400

    Add Board.mount() api and tests
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- test/johnny-five_test.js
+++ test/johnny-five_test.js
@@ -11,9 +11,9 @@ var SerialPort = require("./mock-serial").SerialPort,
 // use:
 // serial.emit( "data", [---] ) to  trigger testable events
 
-console.log( board );
+// console.log( board );
 
-exports["five.Board (static)"] = {
+exports["static"] = {
   "Board.cache": function( test ) {
     test.expect(2);
     test.equal( typeof five.Board.cache, "object", "Board.cache" );
@@ -24,11 +24,34 @@ exports["five.Board (static)"] = {
     test.expect(1);
     test.equal( typeof five.Board.uid, "function", "Board.uid" );
     test.done();
+  },
+  "Board.mount": function( test ) {
+    test.expect(1);
+    test.equal( typeof five.Board.mount, "function", "Board.mount" );
+    test.done();
+  },
+  "Board.mount(obj)": function( test ) {
+    test.expect(2);
+    test.ok( five.Board.mount({ board: board }), "five.Board.mount({ board: board })" );
+    test.deepEqual( five.Board.mount({ board: board }), board, "five.Board.mount({ board: board }) deep equals board" );
+    test.done();
+  },
+  "Board.mount(index)": function( test ) {
+    test.expect(2);
+    test.ok( five.Board.mount(0), "five.Board.mount(0)" );
+    test.deepEqual( five.Board.mount(), board, "five.Board.mount(0)" );
+    test.done();
+  },
+  "Board.mount(/*none*/)": function( test ) {
+    test.expect(2);
+    test.ok( five.Board.mount(), "five.Board.mount()" );
+    test.deepEqual( five.Board.mount(), board, "five.Board.mount() matches board instance" );
+    test.done();
   }
 };
 
 
-exports["five.Board (instance)"] = {
+exports["instance"] = {
   "cache": function( test ) {
     test.expect(1);
     test.equal( five.Board.cache.length, 1, "Board cached" );
@@ -41,12 +64,18 @@ exports["five.Board (instance)"] = {
   },
   "firmata": function( test ) {
     test.expect(1);
-    test.ok( board.firmata, "Board firmata" );
+    test.ok( board.firmata, "Board instance firmata" );
     test.done();
   },
   "id": function( test ) {
     test.expect(1);
-    test.ok( board.id, "Board id" );
+    test.ok( board.id, "Board instance id" );
+    test.done();
+  },
+  "repl": function( test ) {
+    test.expect(2);
+    test.ok( board.repl, "Board instance repl session" );
+    test.ok( board.repl.context, "Board instance repl context" );
     test.done();
   }
 };
