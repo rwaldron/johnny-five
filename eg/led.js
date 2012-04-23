b2ba69695927d12d29ddb0c41b9d20d84175e7aa
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-04-23 11:26:42 -0400

    Add comments to led example
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- eg/led.js
+++ eg/led.js
@@ -7,13 +7,18 @@ board = new five.Board({
 
 board.on("ready", function() {
 
+  // Create a standard `led` hardware instance
   led = new five.Led({
     pin: 13
   });
 
+  // Inject the `pir` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
   board.repl.inject({
     led: led
   });
 
+  // "strobe" the led in 100ms on-off phases
   led.strobe( 100 );
 });
