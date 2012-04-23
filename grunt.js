5a772281091eb53b91ab6014388a43f7b360cc8e
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-04-23 11:25:11 -0400

    Update to deeps; Adds docs task
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- grunt.js
+++ grunt.js
@@ -1,17 +1,34 @@
+var inspect = require("util").inspect;
+
+
 module.exports = function(grunt) {
 
+  var task = grunt.task;
+  var file = grunt.file;
+  var utils = grunt.utils;
+  var log = grunt.log;
+  var verbose = grunt.verbose;
+  var fail = grunt.fail;
+  var option = grunt.option;
+  var config = grunt.config;
+  var template = grunt.template;
+  var _ = utils._;
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
+    pkg: "<json:package.json>",
+
+    docs: {
+      files: ["eg/**/*.js"]
+    },
     test: {
-      files: ['test/**/*.js']
+      files: ["test/**/*.js"]
     },
     lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+      files: ["grunt.js", "lib/**/*.js", "test/**/*.js", "eg/**/*.js"]
     },
     watch: {
-      files: '<config:lint.files>',
-      tasks: 'default'
+      files: "<config:lint.files>",
+      tasks: "default"
     },
     jshint: {
       options: {
@@ -34,6 +51,32 @@ module.exports = function(grunt) {
   });
 
   // Default task.
-  grunt.registerTask('default', 'lint test');
+  grunt.registerTask("default", "lint test");
 
+  grunt.registerMultiTask("docs", "generate simple docs from examples", function() {
+    // Concat specified files.
+    var files = file.expandFiles( this.file.src ),
+        template = _.template( file.read("docs/.template.md") );
+
+    files.forEach(function( filepath ) {
+      var eg = file.read( filepath ),
+          md = filepath.replace("eg", "docs").replace(".js", ".md"),
+          title = filepath;
+
+      [ [ /^.+\//, "" ],
+        [ /\.js/, "" ],
+        [ /\-/, " " ]
+      ].forEach(function( args ) {
+        title = "".replace.apply( title, args );
+      });
+
+      file.write( md, template({
+          title: _.titleize(title),
+          example: eg
+        })
+      );
+    });
+
+    log.writeln("Docs created.");
+  });
 };
