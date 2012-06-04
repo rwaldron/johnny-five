d02f6ee6cfd3bf52b43024cd5e4270b7c211bdd5
Rick Waldron <waldron.rick@gmail.com>
2012-06-04 16:50:12 -0400

    More compass/magnetometer work
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/compass.js
+++ eg/compass.js
@@ -1,17 +1,36 @@
 var five = require("../lib/johnny-five.js"),
-    compass;
+    compass, servo;
 
 five.Board().on("ready", function() {
 
-  compass = new five.Compass({
-    device: "HMC5883L",
-    freq: 500
+  [
+    [ 91, "ccw" ],
+    [ 89, "cw" ]
+
+  ].forEach(function( def ) {
+    five.Servo.prototype[ def[1] ] = function() {
+      this.move( def[0] );
+    };
   });
 
+  // servo = new five.Servo({
+  //   pin: 9,
+  //   type: "continuous"
+  // });
+
+  // this.repl.inject({
+  //   servo: servo
+  // });
+
+  // servo.center();
+
+
+
+  compass = new five.Magnetometer();
 
   compass.on("read", function() {
 
-    console.log( this.heading );
-
+    // console.log( this.axis );
+    console.log( "heading", Math.floor(this.heading) );
   });
 });
