a13b91a788cdab00dd3b522837e189a53b0a3aae
Rick Waldron <waldron.rick@gmail.com>
2012-07-12 13:22:05 -0400

    NodeConf example programs (compass, radar, navigator, slider)
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ eg/nodeconf-compass.js
@@ -0,0 +1,55 @@
+var color = require("colors"),
+    five = require("../lib/johnny-five.js"),
+    colors, mag;
+
+five.Board().on("ready", function() {
+
+  // Create an I2C `Magnetometer` instance
+  mag = new five.Magnetometer();
+
+  // As the heading changes, log heading value
+  mag.on("headingchange", function() {
+    var log;
+
+    log = ( this.bearing.name + " " + Math.floor(this.heading) + "°" );
+
+    console.log(
+      log[ colors[ this.bearing.abbr ] ]
+    );
+  });
+});
+
+colors = {
+  N: "red",
+  NbE: "red",
+  NNE: "red",
+  NEbN: "red",
+  NE: "yellow",
+  NEbE: "yellow",
+  ENE: "yellow",
+  EbN: "yellow",
+  E: "green",
+  EbS: "green",
+  ESE: "green",
+  SEbE: "green",
+  SE: "green",
+  SEbS: "cyan",
+  SSE: "cyan",
+  SbE: "cyan",
+  S: "cyan",
+  SbW: "cyan",
+  SSW: "cyan",
+  SWbS: "blue",
+  SW: "blue",
+  SWbW: "blue",
+  WSW: "blue",
+  WbS: "blue",
+  W: "magenta",
+  WbN: "magenta",
+  WNW: "magenta",
+  NWbW: "magenta",
+  NW: "magenta",
+  NWbN: "magenta",
+  NNW: "magenta",
+  NbW: "red"
+};
