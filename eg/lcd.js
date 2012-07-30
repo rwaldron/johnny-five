96a3dcfad1023f7681704375456b0ebacacbc5fc
Rick Waldron <waldron.rick@gmail.com>
2012-07-30 18:46:05 -0400

    Very rough draft of LCD constructor
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ eg/lcd.js
@@ -0,0 +1,22 @@
+var five = require("../lib/johnny-five"),
+    board, lcd;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  lcd = new five.LCD({
+    // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
+    // Arduino pin # 7    8   9   10  11  12
+    pins: [ 7, 8, 9, 10, 11, 12 ]
+  });
+
+
+  lcd.write("Hello!");
+
+
+  this.repl.inject({
+    lcd: lcd
+  });
+
+});
