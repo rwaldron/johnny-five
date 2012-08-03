97fd659ea05afccf1781f15339b730f39ec52e90
Rebecca Murphey <rmurphey@gmail.com>
2012-08-03 00:39:39 -0400

    Additional updatesto LCD rewrite; fourBitMode => bitMode

--- eg/lcd.js
+++ eg/lcd.js
@@ -9,11 +9,11 @@ board.on("ready", function() {
     // LCD pin name  RS  EN  DB4 DB5 DB6 DB7
     // Arduino pin # 7    8   9   10  11  12
     pins: [ 7, 8, 9, 10, 11, 12 ],
-    fourBitMode: true
+    bitMode: 4
   });
 
 
-  lcd.write('Hi! ' + new Date().getTime());
+  lcd.write("Hi! " + new Date().getTime());
 
   this.repl.inject({
     lcd: lcd
