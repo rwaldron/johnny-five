94a4245339009c373e6f8dbcf63ab3bfef17d2de
Irene Ros <irene@bocoup.com>
2012-08-03 18:00:58 -0400

    Adding change event and updating example to showcase new event API

--- eg/nunchuk.js
+++ eg/nunchuk.js
@@ -8,13 +8,31 @@ board.on("ready", function() {
   // Create a new `nunchuk` hardware instance.
   nunchuk = new five.Nunchuk({
     device: "RVL-004",
-    freq: 100
+    freq: 100,
+    threshold : 100
   });
 
+  nunchuk.on( "read", function( err, e ){ 
+    // console.log(e.type);
+  } );
+  
   // Nunchuk Event API
-  nunchuk.on("read", function() {
-    console.log( this.joystick );
-    console.log( this.accelerometer );
+  nunchuk.joystick.on( "change", function( e ) {
+    console.log( "joystick " + e.axis , e.target[ e.axis ], e.axis, e.direction );
+  });
+
+  nunchuk.accelerometer.on( "change", function( e ) {
+    console.log( "accelerometer " + e.axis , e.target[ e.axis ], e.axis, e.direction );
+  });
+
+  nunchuk.on( "down", function( e ) {
+    console.log( "down", e.target.which, e.target.isUp, e.target.isDown );
+  });
+  nunchuk.on( "up", function( e ) {
+    console.log( "up", e.target.which, e.target.isUp, e.target.isDown );
+  });
+  nunchuk.on( "hold", function( e ) {
+    console.log( "hold", e.target.which, e.target.isUp, e.target.isDown );
   });
 
 // Further reading
