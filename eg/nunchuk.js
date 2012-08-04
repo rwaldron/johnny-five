8bbcca00d7518b39ca0e804bc768d371ce65b4a8
Rick Waldron <waldron.rick@gmail.com>
2012-08-04 12:39:05 -0400

    Update Nunchuk example program; Event documentation, simplified constructor
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/nunchuk.js
+++ eg/nunchuk.js
@@ -7,33 +7,84 @@ board.on("ready", function() {
 
   // Create a new `nunchuk` hardware instance.
   nunchuk = new five.Nunchuk({
-    device: "RVL-004",
-    freq: 100,
-    threshold : 100
+    freq: 50
   });
 
-  nunchuk.on( "read", function( err, e ){ 
-    // console.log(e.type);
-  } );
-  
+
   // Nunchuk Event API
-  nunchuk.joystick.on( "change", function( e ) {
-    console.log( "joystick " + e.axis , e.target[ e.axis ], e.axis, e.direction );
+  //
+
+  // "read" (nunchuk)
+  //
+  // Fired when the joystick detects a change in
+  // axis position.
+  //
+  // nunchuk.on( "read", function( err ) {
+
+  // });
+
+  // "change", "axischange" (joystick)
+  //
+  // Fired when the joystick detects a change in
+  // axis position.
+  //
+  nunchuk.joystick.on( "change", function( err, event ) {
+    console.log(
+      "joystick " + event.axis,
+      event.target[ event.axis ],
+      event.axis, event.direction
+    );
   });
 
-  nunchuk.accelerometer.on( "change", function( e ) {
-    console.log( "accelerometer " + e.axis , e.target[ e.axis ], e.axis, e.direction );
+  // "change", "axischange" (accelerometer)
+  //
+  // Fired when the accelerometer detects a change in
+  // axis position.
+  //
+  nunchuk.accelerometer.on( "change", function( err, event ) {
+    console.log(
+      "accelerometer " + event.axis,
+      event.target[ event.axis ],
+      event.axis, event.direction
+    );
   });
 
-  nunchuk.on( "down", function( e ) {
-    console.log( "down", e.target.which, e.target.isUp, e.target.isDown );
+  // "down"
+  // aliases: "press", "tap", "impact", "hit"
+  //
+  // Fired when any nunchuk button is "down"
+  //
+
+  // "up"
+  // alias: "release"
+  //
+  // Fired when any nunchuk button is "up"
+  //
+
+  // "hold"
+  //
+  // Fired when any nunchuk button is in the "down" state for
+  // a specified amount of time. Defaults to 500ms
+  //
+  // To specify a custom hold time, use the "holdtime"
+  // option of the Nunchuk constructor.
+  //
+
+
+  [ "down", "up", "hold" ].forEach(function( type ) {
+
+    nunchuk.on( type, function( err, event ) {
+      console.log(
+        event.target.which + " is " + type,
+
+        { isUp: event.target.isUp,
+          isDown: event.target.isDown
+        }
+      );
+    });
+
   });
-  nunchuk.on( "up", function( e ) {
-    console.log( "up", e.target.which, e.target.isUp, e.target.isDown );
-  });
-  nunchuk.on( "hold", function( e ) {
-    console.log( "hold", e.target.which, e.target.isUp, e.target.isDown );
-  });
+
 
 // Further reading
 // http://media.pragprog.com/titles/msard/tinker.pdf
