60553ab45eea999d9c2b2d45e88d7cc1cdd1b80a
Rebecca Murphey <rmurphey@gmail.com>
2012-09-09 20:26:09 -0400

    led control heart demo

--- eg/led-matrix.js
+++ eg/led-matrix.js
@@ -1,13 +1,24 @@
-var LedControl = require('../lib/led-control');
+var LedControl = require("../lib/led-control");
 
 var five = require("../lib/johnny-five"),
     board, lc;
 
 board = new five.Board();
 
-board.on('ready', function() {
+board.on("ready", function() {
   var led = new five.Led(13);
   led.on();
+
+  var heart = [
+    "01100110",
+    "10011001",
+    "10000001",
+    "10000001",
+    "01000010",
+    "00100100",
+    "00011000",
+    "00000000"
+  ];
 
   lc = new LedControl({
     data: 2,
@@ -15,6 +26,16 @@ board.on('ready', function() {
     cs: 4,
     devices: 1
   });
+
+  function queue(fn) {
+    process.nextTick( fn );
+  }
+
+  lc.heart = function() {
+    heart.forEach(function(row, rowIndex) {
+      queue( function() { lc.setRow( 0, rowIndex, parseInt( row, 2 ) ); } );
+    });
+  };
 
   lc.on( 0 );
 
