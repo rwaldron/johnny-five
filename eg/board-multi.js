2a1cbca8e3fcdfd57c28dd6980d4d45ffa1d1d49
Rick Waldron <waldron.rick@gmail.com>
2013-06-15 17:06:00 -0400

    Fixes #130. Improved Multi-board support
    Boards.Array (alias: Boards)
    
    See: eg/board-multi.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/board-multi.js
+++ eg/board-multi.js
@@ -1,24 +1,7 @@
-var five = require("../lib/johnny-five.js"),
-    boards;
+var five = require("../lib/johnny-five.js");
 
-boards = [
-  new five.Board({ id: "a" }),
-  new five.Board({ id: "b" })
-];
-
-// Add ready event handlers to both boards.
-boards.forEach(function( board ) {
-  board.on("ready", function() {
-    var val = 0;
-
-    console.log( "ready!!!!!" );
-
-    // Set pin 13 to OUTPUT mode
-    this.pinMode( 13, 1 );
-
-    // Create a loop to "flash/blink/strobe" an led
-    this.loop( 50, function() {
-      this.digitalWrite( 13, (val = val ? 0 : 1) );
-    });
+new five.Boards([ "a", "b" ]).on("ready", function(boards) {
+  this.each(function(board) {
+    new five.Led({ pin: 13, board: board }).strobe();
   });
 });
