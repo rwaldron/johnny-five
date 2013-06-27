d9b6efeb6925db88109b3b1b93ce969180e3b69a
Adam Hyland <protonk@gmail.com>
2013-06-27 11:15:57 -0400

    set exponential smoother as default
    * remove getter (didn't add anything but brittleness)
    * default alpha value is quite aggressive

--- eg/accelerometer.js
+++ eg/accelerometer.js
@@ -4,30 +4,7 @@ var five = require("../lib/johnny-five.js"),
 board = new five.Board();
 
 board.on("ready", function() {
-  // exponential smoother
-  function exponential() {
-    var alpha = this.alpha;
 
-    this.axes.forEach(function( axis, index ) {
-      var diff, priorsm;
-      // Exponential smoothing
-      // see http://people.duke.edu/~rnau/411avg.htm
-      // bootstrap for initial state
-      if ( this.initial ) {
-        this.update("smooth", axis, this.get("accel", axis, 0));
-        this.initial = false;
-      } else {
-
-        priorsm = this.get("smooth", axis, 0);
-        // diff last stage smoothing and real output
-        diff = this.get("accel", axis, 0) - priorsm;
-        // update current smooth 
-        this.update("smooth", axis, priorsm + alpha * diff);
-      }
-    }, this);
-
-    return this.get("smooth");
-  }
   // Create a new `Accelerometer` hardware instance.
   //
   // Supported devices:
@@ -47,7 +24,6 @@ board.on("ready", function() {
     pins: [ "A3", "A4", "A5" ],
     freq: 100,
     axes: ["x", "y"],
-    smoother: exponential,
     threshold: 0.2
   });
 
