a91cb56e8cd49c9148c68c6492a98e5b161988b0
Rick Waldron <waldron.rick@gmail.com>
2013-07-14 18:08:18 -0400

    Refactoring Stepper code
    - Allow an array of pins (in addition to object)
    - Create reasonable defaults where possible
    - Adds stepper-sweep example
    - General code refactoring
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/stepper-driver.js
+++ eg/stepper-driver.js
@@ -2,27 +2,33 @@ var five = require("../lib/johnny-five");
 
 var board = new five.Board();
 
-board.on('ready', function() {
-  var stepperConfig = {
-    type: board.firmata.STEPPER.TYPE.DRIVER,
+board.on("ready", function() {
+
+  // var stepper = new five.Stepper({
+  //   type: five.Stepper.TYPE.DRIVER,
+  //   stepsPerRev: 200,
+  //   pins: {
+  //     step: 11,
+  //     dir: 12
+  //   }
+  // });
+
+
+  var stepper = new five.Stepper({
+    type: five.Stepper.TYPE.DRIVER,
     stepsPerRev: 200,
-    pins: {
-      step: 13,
-      dir: 12
-    }
-  };
+    pins: [ 11, 12 ]
+  });
 
-  var stepper = new five.Stepper(stepperConfig);
+  // Make 10 full revolutions counter-clockwise at 180 rpm with acceleration and deceleration
+  stepper.rpm(180).ccw().accel(1600).decel(1600).step(2000, function() {
 
-  // make 10 full revolutions counter-clockwise at 180 rpm with acceleration and deceleration
-  stepper.rpm(180).direction(board.firmata.STEPPER.DIRECTION.CCW).accel(1600).decel(1600).step(2000, function() {
-    console.log("done moving CCW")
+    console.log("Done moving CCW");
 
     // once first movement is done, make 10 revolutions clockwise at previously
     //      defined speed, accel, and decel by passing an object into stepper.step
-    stepper.step({ steps:2000, direction: board.firmata.STEPPER.DIRECTION.CCW }, function() {
-        console.log('done moving CW');
+    stepper.step({ steps: 2000, direction: five.Stepper.DIRECTION.CW }, function() {
+      console.log("Done moving CW");
     });
-
   });
-})
+});
---
+++ eg/stepper-sweep.js
@@ -0,0 +1,20 @@
+var five = require("../lib/johnny-five");
+
+five.Board().on("ready", function() {
+  var stepper, k = 0;
+
+  stepper = new five.Stepper({
+    type: five.Stepper.TYPE.DRIVER,
+    stepsPerRev: 200,
+    pins: [ 11, 12 ]
+  });
+
+  function sweep() {
+    // 200 stepsPerRev / 2 = 100 (180degree sweeps)
+    stepper[ ++k % 2 === 0 ? "ccw" : "cw" ]().step(100, function() {
+      sweep();
+    });
+  }
+
+  sweep();
+});
