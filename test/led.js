11222afeec9d2a3c53b2777dbabb2af1374214c3
Rick Waldron <waldron.rick@gmail.com>
2013-08-08 12:08:31 -0400

    Fixes bug where PWM Leds start with 0 and therefore on() sends a 0 value. Includes tests
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- test/led.js
+++ test/led.js
@@ -18,7 +18,122 @@ board.pins = Board.Pins( board );
 
 // END
 
-exports["Led"] = {
+exports["Led - Digital"] = {
+  setUp: function( done ) {
+
+    this.led = new Led({ pin: 13, board: board });
+
+    this.proto = [
+      { name: "on" },
+      { name: "off" },
+      { name: "toggle" },
+      { name: "brightness" },
+      { name: "pulse" },
+      { name: "fade" },
+      { name: "fadeIn" },
+      { name: "fadeOut" },
+      { name: "strobe" },
+      { name: "blink" },
+      { name: "stop" }
+    ];
+
+    this.instance = [
+      { name: "id" },
+      { name: "pin" },
+      { name: "value" },
+      { name: "interval" }
+    ];
+
+    done();
+  },
+
+  shape: function( test ) {
+    test.expect( this.proto.length + this.instance.length );
+
+    this.proto.forEach(function( method ) {
+      test.equal( typeof this.led[ method.name ], "function" );
+    }, this);
+
+    this.instance.forEach(function( property ) {
+      test.notEqual( typeof this.led[ property.name ], "undefined" );
+    }, this);
+
+    test.done();
+  },
+
+  on: function( test ) {
+    test.expect(1);
+
+    this.led.on();
+    test.deepEqual( serial.lastWrite, [ 145, 96, 1 ] );
+
+    test.done();
+  },
+
+  off: function( test ) {
+    test.expect(2);
+
+    this.led.off();
+    test.deepEqual( serial.lastWrite, [ 145, 64, 1 ] );
+
+    this.led.on();
+    test.deepEqual( serial.lastWrite, [ 145, 96, 1 ] );
+
+    test.done();
+  },
+
+  toggle: function( test ) {
+    test.expect(2);
+
+    this.led.off();
+    this.led.toggle();
+
+    test.deepEqual( serial.lastWrite, [ 145, 96, 1 ] );
+
+    this.led.toggle();
+    test.deepEqual( serial.lastWrite, [ 145, 64, 1 ] );
+
+    test.done();
+  },
+
+  strobe: function( test ) {
+    var captured = [];
+    var startAt = Date.now();
+
+    test.expect(1);
+
+    this.led.off();
+    this.led.strobe(100);
+
+    var interval = setInterval(function() {
+
+      captured.push( serial.lastWrite.slice() );
+
+      if ( Date.now() > startAt + 500 ) {
+        clearInterval( interval );
+
+        test.deepEqual( captured, [
+          [ 145, 96, 1 ],
+          [ 145, 64, 1 ],
+          [ 145, 96, 1 ],
+          [ 145, 64, 1 ],
+          [ 145, 96, 1 ]
+        ]);
+
+        test.done();
+      }
+    }, 100);
+  },
+
+  blink: function( test ) {
+    test.expect(1);
+    test.equal( this.led.blink, this.led.strobe );
+    test.done();
+  }
+};
+
+
+exports["Led - PWM (Analog)"] = {
   setUp: function( done ) {
 
     this.led = new Led({ pin: 11, board: board });
@@ -65,7 +180,7 @@ exports["Led"] = {
     test.expect(1);
 
     this.led.on();
-    test.deepEqual( serial.lastWrite, [ 145, 72, 1 ] );
+    test.deepEqual( serial.lastWrite, [ 235, 127, 1 ] );
 
     test.done();
   },
@@ -74,10 +189,10 @@ exports["Led"] = {
     test.expect(2);
 
     this.led.off();
-    test.deepEqual( serial.lastWrite, [ 145, 64, 1 ] );
+    test.deepEqual( serial.lastWrite, [ 235, 0, 0 ] );
 
     this.led.on();
-    test.deepEqual( serial.lastWrite, [ 145, 72, 1 ] );
+    test.deepEqual( serial.lastWrite, [ 235, 127, 1 ] );
 
     test.done();
   },
@@ -88,10 +203,10 @@ exports["Led"] = {
     this.led.off();
     this.led.toggle();
 
-    test.deepEqual( serial.lastWrite, [ 145, 72, 1 ] );
+    test.deepEqual( serial.lastWrite, [ 235, 127, 1 ] );
 
     this.led.toggle();
-    test.deepEqual( serial.lastWrite, [ 145, 64, 1 ] );
+    test.deepEqual( serial.lastWrite, [ 235, 0, 0 ] );
 
     test.done();
   },
@@ -148,25 +263,21 @@ exports["Led"] = {
       if ( Date.now() > startAt + 500 ) {
         clearInterval( interval );
 
+        // NOTE: Strobe will always switch to digital!!
         test.deepEqual( captured, [
           [ 145, 72, 1 ],
-          [ 145, 64, 1 ],
+          [ 145, 96, 1 ],
           [ 145, 72, 1 ],
-          [ 145, 64, 1 ],
+          [ 145, 96, 1 ],
           [ 145, 72, 1 ]
         ]);
 
         test.done();
       }
     }, 100);
-  },
-
-  blink: function( test ) {
-    test.expect(1);
-    test.equal( this.led.blink, this.led.strobe );
-    test.done();
   }
 };
+
 
 // TODO
 // exports["Led.Array"] = {
