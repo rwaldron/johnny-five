f0aacc0e360fc4942eb1c336f2f945270aa2aa25
Rick Waldron <waldron.rick@gmail.com>
2013-12-17 14:14:21 -0500

    dreams of destructuring
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- eg/stepper-four_wire.js
+++ eg/stepper-four_wire.js
@@ -1,10 +1,10 @@
 var five = require("../lib/johnny-five");
-
 var board = new five.Board();
+var Stepper = Stepper;
 
 board.on('ready', function() {
   var stepperConfig = {
-    type: board.firmata.STEPPER.TYPE.FOUR_WIRE,
+    type: Stepper.TYPE.FOUR_WIRE,
     stepsPerRev: 200,
     pins: {
       motor1: 10,
@@ -14,17 +14,17 @@ board.on('ready', function() {
     }
   };
 
-  var stepper = new five.Stepper(stepperConfig);
+  var stepper = new Stepper(stepperConfig);
 
   // make 10 full revolutions counter-clockwise at 180 rpm with acceleration and deceleration
-  stepper.rpm(180).direction(board.firmata.STEPPER.DIRECTION.CCW).accel(1600).decel(1600).step(2000, function() {
+  stepper.rpm(180).direction(Stepper.DIRECTION.CCW).accel(1600).decel(1600).step(2000, function() {
     console.log("done moving CCW");
 
     // once first movement is done, make 10 revolutions clockwise at previously
     //      defined speed, accel, and decel by passing an object into stepper.step
     stepper.step({
       steps: 2000,
-      direction: board.firmata.STEPPER.DIRECTION.CCW
+      direction: Stepper.DIRECTION.CCW
     }, function() {
       console.log('done moving CW');
     });
