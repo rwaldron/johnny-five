bddb4b84db89acad886fc6f12c7b68490fa63b38
Donovan Buck <donovan@donovan.bz>
2014-01-11 23:22:33 -0600

    Remove unnecessary tests

--- test/motor.js
+++ test/motor.js
@@ -446,14 +446,6 @@ exports["Motor: Directional - Three Pin"] = {
       name: "start"
     }, {
       name: "stop"
-    }, {
-      name: "forward"
-    }, {
-      name: "fwd"
-    }, {
-      name: "reverse"
-    }, {
-      name: "rev"
     }];
 
     this.instance = [{
