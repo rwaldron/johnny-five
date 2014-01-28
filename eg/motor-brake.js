9b7e18d8467b895150a4aecae3400391111b51fd
Donovan Buck <donovan@donovan.bz>
2014-01-28 14:04:49 -0600

    First commit of eg, fez and docs for motor
    Confession: Breadboard/motor-brake.* and breadboard/motor-current.* are
    just copies of breadboard/motor-directional.*

---
+++ eg/motor-brake.js
@@ -0,0 +1,66 @@
+var five = require("../lib/johnny-five.js"),
+  board = new five.Board();
+
+board.on("ready", function() {
+  var motor;
+  /*
+    Arduino Motor Shield R3
+      Motor A
+        pwm: 3
+        dir: 12
+        brake: 9
+
+      Motor B
+        pwm: 11
+        dir: 13
+        brake: 8
+
+   */
+
+  motor = new five.Motor({
+    pins: {
+      pwm: 3,
+      dir: 12,
+      brake: 9
+    }
+  });
+
+  board.repl.inject({
+    motor: motor
+  });
+
+  motor.on("start", function(err, timestamp) {
+    console.log("start", timestamp);
+  });
+
+  motor.on("stop", function(err, timestamp) {
+    console.log("automated stop on timer", timestamp);
+  });
+
+  motor.on("brake", function(err, timestamp) {
+    console.log("automated brake on timer", timestamp);
+  });
+  
+  motor.on("forward", function(err, timestamp) {
+    console.log("forward", timestamp);
+
+    // demonstrate switching to reverse after 5 seconds
+    board.wait(5000, function() {
+      motor.reverse(150);
+    });
+  });
+
+  motor.on("reverse", function(err, timestamp) {
+    console.log("reverse", timestamp);
+
+    // demonstrate stopping after 5 seconds
+    board.wait(5000, function() {
+      
+      // Apply the brake for 500ms and call stop()
+      motor.brake(500);
+    });
+  });
+
+  // set the motor going forward full speed
+  motor.forward(255);
+});
---
+++ eg/motor-current.js
@@ -0,0 +1,80 @@
+var five = require("../lib/johnny-five.js"),
+  board = new five.Board();
+
+board.on("ready", function() {
+  var motor;
+  /*
+    Arduino Motor Shield R3
+      Motor A
+        pwm: 3
+        dir: 12
+        brake: 9
+        current: "A0"
+
+      Motor B
+        pwm: 11
+        dir: 13
+        brake: 8
+        current: "A1"
+
+   */
+
+  motor = new five.Motor({
+    pins: {
+      pwm: 3,
+      dir: 12,
+      brake: 9
+    },
+    
+    // The current options are passed to a new instance of Sensor
+    current: {
+      pin: "A0",
+      freq: 250,
+      range: [0, 2000],
+      threshold: 50
+    }
+  });
+
+  board.repl.inject({
+    motor: motor
+  });
+
+  motor.current.scale([0, 2000]).on("data", function() {
+    console.log("Motor A: " + this.value + "mA");
+  });
+  
+  motor.on("start", function(err, timestamp) {
+    console.log("start", timestamp);
+  });
+
+  motor.on("stop", function(err, timestamp) {
+    console.log("automated stop on timer", timestamp);
+  });
+
+  motor.on("brake", function(err, timestamp) {
+    console.log("automated brake on timer", timestamp);
+  });
+  
+  motor.on("forward", function(err, timestamp) {
+    console.log("forward", timestamp);
+
+    // demonstrate switching to reverse after 5 seconds
+    board.wait(5000, function() {
+      motor.reverse(150);
+    });
+  });
+
+  motor.on("reverse", function(err, timestamp) {
+    console.log("reverse", timestamp);
+
+    // demonstrate stopping after 5 seconds
+    board.wait(5000, function() {
+      
+      // Apply the brake for 500ms and call stop()
+      motor.brake(500);
+    });
+  });
+
+  // set the motor going forward full speed
+  motor.forward(255);
+});
