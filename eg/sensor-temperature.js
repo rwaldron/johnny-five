cf239182d85fe5503489be2a560cefedda2dc486
Julian Duque <julianduquej@gmail.com>
2014-05-13 16:34:10 -0500

    Add LM35 example

--- eg/sensor-temperature.js
+++ eg/sensor-temperature.js
@@ -4,8 +4,12 @@ five.Board().on("ready", function() {
   var sensor = new five.Sensor("A0");
 
   sensor.on("data", function() {
-    var voltage = this.value * 0.004882814;
-    var celsius = (voltage - 0.5) * 100;
+    // LM35
+    // var celsius = (5 * value * 100) / 1024;
+
+    // TMP36
+    var celsius = ((this.value * 0.004882814) - 0.5) * 100;
+
     var fahrenheit = celsius * (9 / 5) + 32;
 
     console.log(celsius + "°C", fahrenheit + "°F");
@@ -14,4 +18,5 @@ five.Board().on("ready", function() {
 
 // @markdown
 // - [TMP36 - Temperature Sensor](https://www.sparkfun.com/products/10988)
+// - [LM35 - Temperature Sensor](http://www.ti.com/product/lm35)
 // @markdown
