73419f13fdb6a10cc0362a108375c062721b612d
Julian Duque <julianduquej@gmail.com>
2014-07-14 21:40:53 -0500

    Change beat to fractional values

--- eg/piezo.js
+++ eg/piezo.js
@@ -16,26 +16,26 @@ board.on("ready", function() {
     // The first argument is the note (null means "no note")
     // The second argument is the length of time (beat) of the note (or non-note)
     song: [
-      ["C4", 1],
-      ["D4", 1],
-      ["F4", 1],
-      ["D4", 1],
+      ["C4", 1/4],
+      ["D4", 1/4],
+      ["F4", 1/4],
+      ["D4", 1/4],
+      ["A4", 1/4],
+      [null, 1/4],
       ["A4", 1],
-      [null, 1],
-      ["A4", 4],
-      ["G4", 4],
-      [null, 2],
-      ["C4", 1],
-      ["D4", 1],
-      ["F4", 1],
-      ["D4", 1],
       ["G4", 1],
-      [null, 1],
-      ["G4", 4],
-      ["F4", 4],
-      [null, 2]
+      [null, 1/2],
+      ["C4", 1/4],
+      ["D4", 1/4],
+      ["F4", 1/4],
+      ["D4", 1/4],
+      ["G4", 1/4],
+      [null, 1/4],
+      ["G4", 1],
+      ["F4", 1],
+      [null, 1/2]
     ],
-    tempo: 250
+    tempo: 100
   });
 
 });
