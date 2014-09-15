c7d0dba1a0eee205279d06fb553aa7679d264510
David Resseguie <david@resseguie.com>
2014-09-15 09:40:24 -0400

    Feature implementation and bug fixes on RBG.
    Adds isOn property for RGB that looks to see if ANY of the colors are
    on. When calling .on(), it leaves the led as-is if it’s already on.
    
    Adds chaining capability to .on() and .off()
    
    Supports saving previous color as requested in #375
    
    Updates toggle/strobe/stop functions to work with RGB.
    
    Removes functions not applicable to RBG
    (fade, fadeIn, fadeOut, pulse, brightness)
    Note, we could potentially implement these later with appropriate calculations.

--- eg/led-rgb-keypress.js
+++ eg/led-rgb-keypress.js
@@ -1,4 +1,5 @@
-var five = require("../lib/johnny-five.js");
+var five = require("../lib/johnny-five.js"),
+  keypress = require("keypress");
 
 
 five.Board().on("ready", function() {
@@ -31,6 +32,30 @@ five.Board().on("ready", function() {
   led.on();
   led.color("#FF0000");
 
-  led.blink(1000);
+  // Listen for user input to change the RGB color
+  process.stdin.resume();
+  process.stdin.setEncoding("utf8");
+  process.stdin.setRawMode(true);
+
+  var keymap = {
+    r: "#FF0000", // red
+    g: "#00FF00", // green
+    b: "#0000FF", // blue
+    w: "#FFFFFF" // white
+  };
+
+  process.stdin.on("keypress", function(ch, key) {
+
+    if (!key) {
+      return;
+    }
+
+    if (keymap[key.name]) {
+      led.color(keymap[key.name]);
+    } else {
+      led.off();
+    }
+
+  });
 
 });
