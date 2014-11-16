b807774f4ddedc3cde44fdcd9b3cf4bc5739208e
Divan Visagie <visagiedivan@gmail.com>
2014-11-16 17:31:07 +0200

    attempt joystick change test

--- test/wii.js
+++ test/wii.js
@@ -2,8 +2,7 @@ var five = require("../lib/johnny-five.js"),
   sinon = require("sinon"),
   MockFirmata = require("./mock-firmata"),
   Wii = five.Wii,
-  Board = five.Board,
-  board;
+  Board = five.Board;
 
 function newBoard() {
   return new Board({
@@ -18,10 +17,11 @@ exports["Nunchuk"] = {
   setUp: function(done) {
 
     this.clock = sinon.useFakeTimers();
-    board = newBoard();
+    this.board = newBoard();
+    this.sendI2CReadRequest = sinon.spy(this.board.io, "sendI2CReadRequest");
 
-    this.wii = new Wii.Nunchuk({
-      board: board,
+    this.nunchuk = new Wii.Nunchuk({
+      board: this.board,
       freq: 50
     });
 
@@ -41,28 +41,55 @@ exports["Nunchuk"] = {
     test.expect(this.proto.length + this.instance.length);
 
     this.proto.forEach(function(method) {
-      test.equal(typeof this.wii[method.name], "function");
+      test.equal(typeof this.nunchuk[method.name], "function");
     }, this);
 
     this.instance.forEach(function(property) {
-      test.notEqual(typeof this.wii[property.name], "undefined");
+      test.notEqual(typeof this.nunchuk[property.name], "undefined");
     }, this);
 
     test.done();
   },
 
+  tearDown: function(done) {
+    this.clock.restore();
+    this.sendI2CReadRequest.restore();
+    done();
+  },
+
   data: function(test) {
     test.expect(1);
 
-    this.wii.on("data", function() {
+    this.nunchuk.on("data", function() {
       test.ok(true);
     });
     this.clock.tick(50);
     test.done();
   },
 
-  tearDown: function(done) {
-    this.clock.restore();
-    done();
+  "joystick change": function(test) {
+    test.expect(1);
+
+    this.clock.tick(50);
+
+    var callback = this.sendI2CReadRequest.args[0][2];
+    this.nunchuk.joystick.on("change", function(err, event) {
+      console.log(
+        "joystick " + event.axis,
+        event.target[event.axis],
+        event.axis, event.direction
+      );
+
+      test.ok(true);
+
+      test.done();
+    });
+
+    callback([0, 100, 0, 100, 0, 100]);
+    this.clock.tick(500);
+
+    callback([0, 32, 0, 32, 0, 32]);
+    this.clock.tick(500);
   }
+
 };
