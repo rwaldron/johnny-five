4aa42a02f403047343bdec4b9642fdb4119ba3c6
Derek Wheelden <derek.wheelden@gmail.com>
2014-12-06 14:45:38 -0500

    Adding GP2Y0A41SK0F to supported ir-distance devices

--- test/distance.js
+++ test/distance.js
@@ -159,6 +159,40 @@ exports["IR.Distance: GP2Y0A02YK0F"] = {
   }
 };
 
+exports["IR.Distance: GP2Y0A41SK0F"] = {
+  setUp: function(done) {
+    this.clock = sinon.useFakeTimers();
+    this.analogRead = sinon.spy(board.io, "analogRead");
+    this.distance = new Distance({
+      device: "GP2Y0A41SK0F",
+      pin: "A1",
+      board: board
+    });
+
+    done();
+  },
+
+  tearDown: function(done) {
+    this.clock.restore();
+    this.analogRead.restore();
+    done();
+  },
+
+  GP2Y0A41SK0F: function(test) {
+    var callback = this.analogRead.args[0][1];
+
+    test.expect(2);
+
+    // 325 is an actual reading at ~2.5"
+    callback(325);
+
+    test.equals(Math.round(this.distance.cm), 7);
+    test.equals(Math.round(this.distance.inches), 3);
+
+    test.done();
+  }
+};
+
 
 // - GP2Y0A21YK
 //     https://www.sparkfun.com/products/242
@@ -166,3 +200,5 @@ exports["IR.Distance: GP2Y0A02YK0F"] = {
 //     https://www.sparkfun.com/products/8959
 // - GP2Y0A02YK0F
 //     https://www.sparkfun.com/products/8958
+// - GP2Y0A41SK0F
+//     https://www.sparkfun.com/products/12728
