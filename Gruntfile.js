436753e2972b3580c18d1c1e8d9ae6999a69ab12
Rick Waldron <waldron.rick@gmail.com>
2015-01-18 16:46:32 -0500

    Fix description of "examples" task

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask("default", ["jshint", "jscs", "nodeunit"]);
 
-  grunt.registerMultiTask("examples", "generate simple examples from examples", function() {
+  grunt.registerMultiTask("examples", "Generate examples", function() {
     // Concat specified files.
     var entries = JSON.parse(file.read(file.expand(this.data)));
     var readme = [];
