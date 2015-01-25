d810b867960cceb6572abda5e067a45c8bace8d9
Brian Genisio <BrianGenisio@Gmail.com>
2015-01-25 14:03:38 -0500

    Setting up a basic controller setup for the mma7361

---
+++ eg/accelerometer-mma7361.js
@@ -0,0 +1,27 @@
+var five = require("../lib/johnny-five.js");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var accelerometer = new five.Accelerometer({
+    controller: "MMA7361",
+    pins: ["A0", "A1", "A2"],
+    sleepPin: 13
+  });
+
+  accelerometer.on("xdata", function(data) {
+    console.log(data);
+  });
+
+  accelerometer.on("change", function() {
+    console.log("accelerometer");
+    console.log("  x            : ", this.x);
+    console.log("  y            : ", this.y);
+    console.log("  z            : ", this.z);
+    console.log("  pitch        : ", this.pitch);
+    console.log("  roll         : ", this.roll);
+    console.log("  acceleration : ", this.acceleration);
+    console.log("  inclination  : ", this.inclination);
+    console.log("  orientation  : ", this.orientation);
+    console.log("--------------------------------------");
+  });
+});
