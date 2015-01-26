14be09b7198178cde7eb60ffc97180a2691b6037
Rick Waldron <waldron.rick@gmail.com>
2015-01-26 13:39:20 -0500

    Occupied pins must account for controller and address variants: multiple pin support

--- test/board.component.js
+++ test/board.component.js
@@ -25,143 +25,143 @@ exports["Board.Component"] = {
     done();
   },
 
-  // callThroughs: function(test) {
-  //   test.expect(5);
+  callThroughs: function(test) {
+    test.expect(5);
 
-  //   var a = sinon.spy(Board, "mount");
-  //   var b = sinon.spy(Board.Pins, "normalize");
-  //   var opts = {};
+    var a = sinon.spy(Board, "mount");
+    var b = sinon.spy(Board.Pins, "normalize");
+    var opts = {};
 
-  //   Board.purge();
+    Board.purge();
 
-  //   var board = new Board({
-  //     io: this.io,
-  //     debug: false,
-  //     repl: false
-  //   });
+    var board = new Board({
+      io: this.io,
+      debug: false,
+      repl: false
+    });
 
-  //   Board.Component.call({}, opts);
+    Board.Component.call({}, opts);
 
-  //   test.equal(a.calledOnce, true);
-  //   test.equal(a.getCall(0).args[0], opts);
+    test.equal(a.calledOnce, true);
+    test.equal(a.getCall(0).args[0], opts);
 
-  //   test.equal(b.calledOnce, true);
-  //   test.equal(b.getCall(0).args[0], opts);
-  //   test.equal(b.getCall(0).args[1].id, board.id);
+    test.equal(b.calledOnce, true);
+    test.equal(b.getCall(0).args[0], opts);
+    test.equal(b.getCall(0).args[1].id, board.id);
 
 
-  //   a.restore();
-  //   b.restore();
+    a.restore();
+    b.restore();
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // emptyOptsInitialization: function(test) {
-  //   test.expect(3);
+  emptyOptsInitialization: function(test) {
+    test.expect(3);
 
-  //   var component = new Board.Component();
+    var component = new Board.Component();
 
-  //   test.equal(component.id, null);
-  //   test.equal(component.board, this.board);
-  //   test.equal(component.io, this.io);
+    test.equal(component.id, null);
+    test.equal(component.board, this.board);
+    test.equal(component.io, this.io);
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // callEmptyOptsInitialization: function(test) {
-  //   test.expect(3);
+  callEmptyOptsInitialization: function(test) {
+    test.expect(3);
 
-  //   var component = {};
+    var component = {};
 
-  //   Board.Component.call(component);
+    Board.Component.call(component);
 
-  //   test.equal(component.id, null);
-  //   test.equal(component.board, this.board);
-  //   test.equal(component.io, this.io);
+    test.equal(component.id, null);
+    test.equal(component.board, this.board);
+    test.equal(component.io, this.io);
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // explicitIdInitialization: function(test) {
-  //   test.expect(1);
+  explicitIdInitialization: function(test) {
+    test.expect(1);
 
-  //   var component = new Board.Component({
-  //     id: "foo"
-  //   });
+    var component = new Board.Component({
+      id: "foo"
+    });
 
-  //   test.equal(component.id, "foo");
+    test.equal(component.id, "foo");
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // callExplicitIdInitialization: function(test) {
-  //   test.expect(1);
+  callExplicitIdInitialization: function(test) {
+    test.expect(1);
 
-  //   var component = {};
+    var component = {};
 
-  //   Board.Component.call(component, {
-  //     id: "foo"
-  //   });
+    Board.Component.call(component, {
+      id: "foo"
+    });
 
-  //   test.equal(component.id, "foo");
+    test.equal(component.id, "foo");
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // singlePinInitialization: function(test) {
-  //   test.expect(1);
+  singlePinInitialization: function(test) {
+    test.expect(1);
 
-  //   var component = new Board.Component({
-  //     pin: 1
-  //   });
+    var component = new Board.Component({
+      pin: 1
+    });
 
-  //   test.equal(component.pin, 1);
+    test.equal(component.pin, 1);
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // multiPinInitialization: function(test) {
-  //   test.expect(1);
+  multiPinInitialization: function(test) {
+    test.expect(1);
 
-  //   var component = new Board.Component({
-  //     pins: [1, 2, 3]
-  //   });
+    var component = new Board.Component({
+      pins: [1, 2, 3]
+    });
 
-  //   test.deepEqual(component.pins, [1, 2, 3]);
+    test.deepEqual(component.pins, [1, 2, 3]);
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // explicitPinNormalized: function(test) {
-  //   test.expect(1);
+  explicitPinNormalized: function(test) {
+    test.expect(1);
 
-  //   this.board.io.name = "Foo";
-  //   this.board.io.normalize = function(pin) {
-  //     return Math.pow(pin, 2);
-  //   };
+    this.board.io.name = "Foo";
+    this.board.io.normalize = function(pin) {
+      return Math.pow(pin, 2);
+    };
 
-  //   var component = new Board.Component({
-  //     pin: 2
-  //   });
+    var component = new Board.Component({
+      pin: 2
+    });
 
-  //   test.equal(component.pin, 4);
+    test.equal(component.pin, 4);
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
-  // componentRegistered: function(test) {
-  //   test.expect(2);
+  componentRegistered: function(test) {
+    test.expect(2);
 
-  //   test.equal(this.board.register.length, 0);
+    test.equal(this.board.register.length, 0);
 
-  //   var component = new Board.Component({
-  //     pin: 2
-  //   });
+    var component = new Board.Component({
+      pin: 2
+    });
 
-  //   test.equal(this.board.register.length, 1);
+    test.equal(this.board.register.length, 1);
 
-  //   test.done();
-  // },
+    test.done();
+  },
 
   componentPinOccupiedWarning: function(test) {
     test.expect(5);
@@ -190,7 +190,7 @@ exports["Board.Component"] = {
     test.done();
   },
 
-  componentAddressOccupiedWarning: function(test) {
+  componentPinAddressOccupiedWarning: function(test) {
     test.expect(7);
 
     var component = {};
@@ -230,7 +230,7 @@ exports["Board.Component"] = {
     test.done();
   },
 
-  componentControllerOccupiedWarning: function(test) {
+  componentPinControllerOccupiedWarning: function(test) {
     test.expect(7);
 
     var component = {};
@@ -336,4 +336,67 @@ exports["Board.Component"] = {
     test.equal(component.board.occupied.length, 0);
     test.done();
   },
+
+  componentPinsOccupiedWarning: function(test) {
+    test.expect(12);
+
+    var component = {};
+
+    Board.Component.call(component, {
+      pins: { a: 1, b: 2, c: 3 }
+    });
+
+    var spy = sinon.spy(component.board, "warn");
+
+    test.equal(component.board.occupied.length, 3);
+    test.deepEqual(component.board.occupied[0], {
+      value: 1, type: "pin"
+    });
+
+    test.deepEqual(component.board.occupied[1], {
+      value: 2, type: "pin"
+    });
+
+    test.deepEqual(component.board.occupied[2], {
+      value: 3, type: "pin"
+    });
+
+    // This will be rejected since the pin is already
+    // occupied for this controller.
+    Board.Component.call(component, {
+      pin: 1
+    });
+
+    // This will be rejected since the pin is already
+    // occupied for this controller.
+    Board.Component.call(component, {
+      pin: 2
+    });
+
+    // This will be rejected since the pin is already
+    // occupied for this controller.
+    Board.Component.call(component, {
+      pin: 3
+    });
+
+    // This will be rejected since the pin is already
+    // occupied for this controller.
+    Board.Component.call(component, {
+      pins: { a: 1, b: 2, c: 3 }
+    });
+
+    // 1, 2, 3 + 3
+    test.equal(spy.callCount, 6);
+    test.deepEqual(spy.getCall(0).args, [ "Component", "pin: 1 is already in use" ]);
+    test.deepEqual(spy.getCall(1).args, [ "Component", "pin: 2 is already in use" ]);
+    test.deepEqual(spy.getCall(2).args, [ "Component", "pin: 3 is already in use" ]);
+    test.deepEqual(spy.getCall(3).args, [ "Component", "pin: 1 is already in use" ]);
+    test.deepEqual(spy.getCall(4).args, [ "Component", "pin: 2 is already in use" ]);
+    test.deepEqual(spy.getCall(5).args, [ "Component", "pin: 3 is already in use" ]);
+
+    test.equal(component.board.occupied.length, 3);
+
+    test.done();
+  },
+
 };
