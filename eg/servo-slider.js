0f8f431eab0490fe21147960bac70c6148d66e73
David Resseguie <david@resseguie.com>
2015-04-04 23:43:10 -0400

    servo-slider example consolidation

---
+++ eg/servo-slider.js
@@ -0,0 +1,28 @@
+var five = require("../lib/johnny-five.js"),
+  board, slider, servo, scalingRange;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  scalingRange = [0, 170];
+
+  slider = new five.Sensor({
+    pin: "A0",
+    freq: 50
+  });
+
+  servo = new five.Servo({
+    pin: 10,
+    range: scalingRange
+  });
+
+
+  // The slider's value will be scaled to match the servo's movement range
+
+  slider.scale(scalingRange).on("slide", function(err, value) {
+
+    servo.to(Math.floor(this.value));
+
+  });
+});
