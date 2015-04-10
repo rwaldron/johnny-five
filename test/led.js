19dd2b74a83440f21bda2e9b32d7e4e92767f312
David Resseguie <david@resseguie.com>
2015-04-10 11:16:47 -0400

    test pin assignment for constructors in Led.RGB

--- test/led.js
+++ test/led.js
@@ -925,7 +925,7 @@ exports["Led.RGB"] = {
   params: function(test) {
     var led;
 
-    test.expect(4);
+    test.expect(16);
 
     // Test object constructor
     test.doesNotThrow(function() {
@@ -937,22 +937,36 @@ exports["Led.RGB"] = {
         }
       });
     });
+    test.equal(led.red.pin, 9);
+    test.equal(led.green.pin, 10);
+    test.equal(led.blue.pin, 11);
     
     // Test object constructor with array
     test.doesNotThrow(function() {
       led = new Led.RGB({
         pins: [9, 10, 11]
       });
+
     });
+    test.equal(led.red.pin, 9);
+    test.equal(led.green.pin, 10);
+    test.equal(led.blue.pin, 11);
+
     // Test array constructor
     test.doesNotThrow(function() {
       led = new Led.RGB([9, 10, 11]);
     });
+    test.equal(led.red.pin, 9);
+    test.equal(led.green.pin, 10);
+    test.equal(led.blue.pin, 11);
 
     // Test three param constructor
     test.doesNotThrow(function() {
       led = new Led.RGB(9, 10, 11);
     });
+    test.equal(led.red.pin, 9);
+    test.equal(led.green.pin, 10);
+    test.equal(led.blue.pin, 11);
 
     test.done();
 
