101712e5b2d17de0e3548f39b9b51871c61a6302
Derek Wheelden <derek.wheelden@gmail.com>
2015-04-23 15:37:52 -0400

    Fix HC-SR04 toCm issue, add docs

---
+++ eg/proximity-hcsr04.js
@@ -0,0 +1,19 @@
+var five = require("johnny-five.js"),
+  prox, led;
+
+five.Board().on("ready", function() {
+
+  var proximity = new five.Proximity({
+    controller: "HCSR04",
+    pin: 7
+  });
+
+  proximity.on("data", function(data) {
+    console.log(data.cm + "cm", data.in + "in");
+  });
+
+  proximity.on("change", function(data) {
+    console.log("The obstruction has moved.");
+  });
+
+});
