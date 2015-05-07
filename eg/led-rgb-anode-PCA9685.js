a16b42e9ab7fe4f34903cd2aacf5e9edf1922691
Scott González <scott.gonzalez@gmail.com>
2015-05-06 20:48:55 -0400

    Examples: Add common anode RGB with PCA9685 controller

---
+++ eg/led-rgb-anode-PCA9685.js
@@ -0,0 +1,42 @@
+var five = require("../lib/johnny-five.js");
+
+
+five.Board().on("ready", function() {
+
+  // Initialize the RGB LED
+  var led = new five.Led.RGB({
+    pins: {
+      red: 2,
+      green: 1,
+      blue: 0
+    },
+    isAnode: true,
+    controller: "PCA9685"
+  });
+
+  // RGB LED alternate constructor
+  // This will normalize an array of pins in [r, g, b]
+  // order to an object (like above) that's shaped like:
+  // {
+  //   red: r,
+  //   green: g,
+  //   blue: b
+  // }
+  // var led = new five.Led.RGB({
+  //   pins: [2, 1, 0],
+  //   isAnode: true,
+  //   controller: "PCA9685"
+  // });
+
+  // Add led to REPL (optional)
+  this.repl.inject({
+    led: led
+  });
+
+  // Turn it on and set the initial color
+  led.on();
+  led.color("#FF0000");
+
+  led.blink(1000);
+
+});
