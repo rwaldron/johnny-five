66a7d80b5ed343de6b9e9bedf35c185ba1109ace
Brian Genisio <BrianGenisio@Gmail.com>
2015-10-05 20:42:36 -0400

    Adding altimeter support

---
+++ eg/multi-mpl3115a2.js
@@ -0,0 +1,25 @@
+var five = require("../");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var multi = new five.Multi({
+    controller: "MPL3115A2"
+  });
+
+  multi.on("change", function() {
+    console.log("temperature");
+    console.log("  celsius      : ", this.temperature.celsius);
+    console.log("  fahrenheit   : ", this.temperature.fahrenheit);
+    console.log("  kelvin       : ", this.temperature.kelvin);
+    console.log("--------------------------------------");
+
+    console.log("barometer");
+    console.log("  pressure     : ", this.barometer.pressure);
+    console.log("--------------------------------------");
+
+    console.log("altimeter");
+    console.log("  feet         : ", this.altimeter.feet);
+    console.log("  meters       : ", this.altimeter.meters);
+    console.log("--------------------------------------");
+  });
+});
