daaa0374ccfe88fb1d3a4cd785b832f8e6288d81
Rick Waldron <waldron.rick@gmail.com>
2016-03-11 10:42:32 -0500

    Multi: Adds MS5611 example

--- eg/multi-MS5611.js
+++ eg/multi-MS5611.js
@@ -1,34 +1,3 @@
-<!--remove-start-->
-
-# Multi - MS5611
-
-<!--remove-end-->
-
-
-
-
-
-
-##### Breadboard for "Multi - MS5611"
-
-
-
-![docs/breadboard/multi-MS5611.png](breadboard/multi-MS5611.png)<br>
-
-Fritzing diagram: [docs/breadboard/multi-MS5611.fzz](breadboard/multi-MS5611.fzz)
-
-&nbsp;
-
-
-
-
-Run this example from the command line with:
-```bash
-node eg/multi-MS5611.js
-```
-
-
-```javascript
 var five = require("../");
 var board = new five.Board();
 
@@ -58,24 +27,3 @@ board.on("ready", function() {
     console.log("--------------------------------------");
   });
 });
-
-```
-
-
-
-
-
-
-
-
-&nbsp;
-
-<!--remove-start-->
-
-## License
-Copyright (c) 2012, 2013, 2014 Rick Waldron <waldron.rick@gmail.com>
-Licensed under the MIT license.
-Copyright (c) 2016 The Johnny-Five Contributors
-Licensed under the MIT license.
-
-<!--remove-end-->
