c1d78c42d75b2a88bed9e936461b7cd75a5290e9
pop <pavol.poljak@steinringer.sk>
2016-04-15 10:25:47 +0200

    Added examples and docs for SHT31D multi sensor

---
+++ eg/hygrometer-sht31d.js
@@ -0,0 +1,16 @@
+var five = require("../lib/johnny-five.js");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var hygrometer = new five.Hygrometer({
+    controller: "SHT31D"
+  });
+
+  hygrometer.on("data", function() {
+    console.log(this.relativeHumidity + " %");
+  });
+});
+
+// @markdown
+// - [SHT31D - Humidity Sensor](https://www.adafruit.com/products/2857)
+// @markdown
