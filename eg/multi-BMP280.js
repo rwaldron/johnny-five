672f7836212d5d9ee878e85a797df5b3cb5e82be
Rick Waldron <waldron.rick@gmail.com>
2016-04-19 17:15:06 -0400

    IMU/Multi: BMP280 examples
    Needs diagrams;
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ eg/multi-BMP280.js
@@ -0,0 +1,25 @@
+var five = require("../");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var multi = new five.Multi({
+    controller: "BMP280"
+  });
+
+  multi.on("change", function() {
+    console.log("thermometer");
+    console.log("  celsius      : ", this.thermometer.celsius);
+    console.log("  fahrenheit   : ", this.thermometer.fahrenheit);
+    console.log("  kelvin       : ", this.thermometer.kelvin);
+    console.log("--------------------------------------");
+
+    console.log("barometer");
+    console.log("  pressure     : ", this.barometer.pressure);
+    console.log("--------------------------------------");
+
+    console.log("altimeter");
+    console.log("  feet         : ", this.altimeter.feet);
+    console.log("  meters       : ", this.altimeter.meters);
+    console.log("--------------------------------------");
+  });
+});
