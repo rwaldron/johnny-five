4d88274aa5d3f0bbc234d24ff87602e7b6dc5e15
Derek Wheelden <derek.wheelden@gmail.com>
2016-04-26 20:32:24 -0400

    Move filtration to mixin

--- eg/proximity-filtered.js
+++ eg/proximity-filtered.js
@@ -5,22 +5,19 @@ board.on("ready", function() {
   var proximity = new five.Proximity({
     controller: "GP2Y0A41SK0F",
     pin: "A0",
-    filter: {
-      samples: 5,
-      soften: 2,
-      deviation: 2
-    }
+    filter: true,
+    freq: 500
   });
 
   proximity.on("data", function() {
     console.log("Proximity: ");
     console.log("  cm  : ", this.cm);
     console.log("  in  : ", this.in);
+    console.log("  filtered  : ", this.filtered);
     console.log("-----------------");
   });
 
-  proximity.on("change:filtered", function() {
+  proximity.on("change", function() {
     console.log("The obstruction has moved.");
   });
-
 });
