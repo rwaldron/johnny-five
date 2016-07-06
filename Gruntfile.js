5a455c74610b4f3b5bbf8d0e3baa91b94e86e208
Rick Waldron <waldron.rick@gmail.com>
2016-07-06 10:03:41 -0400

    Gruntfile: add easier to run "quality control" task (see long description)
    Usage: 'grunt qc' or 'grunt qc:<file.ext>' or 'grunt qc:<expr>'
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,6 +129,17 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-nodeunit");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-jsbeautifier");
+  grunt.loadNpmTasks("grunt-jscs");
+
+  // grunt.registerTask("beautify", ["jsbeautifier"]);
+
+  grunt.registerTask("default", ["jshint", "jscs", "nodeunit"]);
+  grunt.registerTask("test", ["jshint", "jscs", "nodeunit:complete"]);
+
   grunt.registerTask("example", "Create an example program, usage: \"grunt expample:<file-name>[.js]\"", function(fileName) {
 
     if (!fileName.endsWith(".js")) {
@@ -173,6 +184,19 @@ module.exports = function(grunt) {
     grunt.task.run("nodeunit:file:" + file);
   });
 
+  grunt.registerTask("qc", "Run JSHint & JSCS checks on a file or files by specifying a file name or glob expression. Usage: 'grunt qc' or 'grunt qc:<file.ext>' or 'grunt qc:<expr>'", function(input) {
+
+    if (input) {
+      primaryFiles.length = 0;
+
+      file.expand(input).forEach(function(file) {
+        primaryFiles.push(file);
+      });
+    }
+
+    grunt.task.run("jshint");
+    grunt.task.run("jscs");
+  });
 
   grunt.registerTask("beautify", "Cleanup a single or limited set of files; usage: 'grunt beautify:file.js' or 'grunt beautify:{file-a.js,file-b.js}' (extension optional)", function(input) {
 
@@ -195,17 +219,6 @@ module.exports = function(grunt) {
     grunt.task.run("nodeunit");
   });
 
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-nodeunit");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-jsbeautifier");
-  grunt.loadNpmTasks("grunt-jscs");
-
-  // grunt.registerTask("beautify", ["jsbeautifier"]);
-
-  grunt.registerTask("default", ["jshint", "jscs", "nodeunit"]);
-  // Explicit test task runs complete set of tests
-  grunt.registerTask("test", ["jshint", "jscs", "nodeunit:complete"]);
 
   grunt.registerMultiTask("examples", "Generate examples", function() {
     // Concat specified files.
