418b0e5eff52fd92a2ac6951847ada65d9ca8838
Rick Waldron <waldron.rick@gmail.com>
2016-07-07 11:09:52 -0400

    Piezo: refactoring piezo_i2c_backpack.ino
    Depends on RingBuf.h
    
    Install via Arduino IDE:
    
    1. Sketch > Include Library > Manage Libraries
    2. Search for "RingBuf", install "RingBuf"
    
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- firmwares/piezo_i2c_backpack.ino
+++ firmwares/piezo_i2c_backpack.ino
@@ -1,6 +1,10 @@
 #include <Wire.h>
+// Sketch > Include Library > Manage Libraries
+// Search for "RingBuf", install "RingBuf"
+#include <RingBuf.h>
 
 #define DEBUG_MODE 0
+
 
 // Address Pins
 #define AD0 11
@@ -14,7 +18,14 @@
 #define NO_TONE 0x00
 #define TONE 0x01
 
-byte buffer[BUFFER_SIZE];
+struct frame {
+  uint8_t command;
+  uint8_t pin;
+  uint16_t hz;
+  uint32_t ms;
+};
+
+RingBuf *buffer = RingBuf_new(sizeof(struct frame), 8);
 
 int addressPins[] = { AD0, AD1 };
 int address = I2C_DEFAULT_ADDRESS;
@@ -37,11 +48,39 @@ void setup() {
     Serial.begin(9600);
   #endif
 
+  for (int i = 0; i < AD0; i++) {
+    pinMode(i, OUTPUT);
+  }
+
   Wire.begin(address);
   Wire.onReceive(onReceive);
 }
 
-void loop() {}
+void loop() {
+  uint8_t numElements = buffer->numElements(buffer);
+
+  if (numElements > 0) {
+    for (int i = 0; i < numElements; i++) {
+      struct frame f;
+
+      buffer->pull(buffer, &f);
+
+      if (f.command == NO_TONE) {
+        noTone(f.pin);
+      }
+
+      if (f.command == TONE) {
+        if (f.ms == 0) {
+          tone(f.pin, f.hz);
+        } else {
+          tone(f.pin, f.hz, f.ms);
+          delay((int)(f.ms * 1.5));
+          noTone(f.pin);
+        }
+      }
+    }
+  }
+}
 
 void onReceive(int howMany) {
 
@@ -49,56 +88,35 @@ void onReceive(int howMany) {
     howMany = BUFFER_SIZE;
   }
 
-  memset(&buffer[0], 0, howMany);
-
-  uint8_t command;
-  uint8_t pin;
-  uint16_t hz;
-  uint32_t ms;
+  uint8_t received[howMany];
 
   for (int i = 0; i < howMany; i++) {
-    buffer[i] = Wire.read();
+    received[i] = Wire.read();
   }
 
-  command = buffer[0];
-  pin = buffer[1];
-  hz = (buffer[2] << 8) | buffer[3];
+  struct frame f;
+
+  f.command = received[0];
+  f.pin = received[1];
+  f.hz = (received[2] << 8) | received[3];
 
   if (howMany == 8) {
-    ms = (buffer[4] << 24) | (buffer[5] << 16) | (buffer[6] << 8) | buffer[7];
+    f.ms = (received[4] << 24) | (received[5] << 16) | (received[6] << 8) | received[7];
   }
+
+  buffer->add(buffer, &f);
 
   #if DEBUG_MODE
     Serial.print("Bytes Received: ");
     Serial.println(howMany);
     Serial.print("command: ");
-    Serial.println(command);
+    Serial.println(f.command);
     Serial.print("pin: ");
-    Serial.println(pin);
+    Serial.println(f.pin);
     Serial.print("hz: ");
-    Serial.println(hz);
+    Serial.println(f.hz);
     Serial.print("ms: ");
-    Serial.println(ms);
+    Serial.println(f.ms);
     Serial.println("--------------------");
   #endif
-
-  pinMode(pin, OUTPUT);
-
-  if (command == NO_TONE) {
-    noTone(pin);
-  }
-
-  if (command == TONE) {
-    if (ms == 0) {
-      tone(pin, hz);
-    } else {
-      tone(pin, hz, ms);
-    }
-  // delay between notes
-  int pauseBetweenNotes = ms * 1.3;
-  delay(pauseBetweenNotes);
-  // stop the tone ?
-  //noTone(pin);
-  }
-
 }
