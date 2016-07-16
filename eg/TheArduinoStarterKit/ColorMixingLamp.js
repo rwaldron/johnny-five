b2b737389605dfac789a1107c1ba9375c43d0499
Larry Fantasy <larry.fantasy@gmail.com>
2016-07-16 19:59:14 -0400

    improve coding style

--- eg/TheArduinoStarterKit/ColorMixingLamp.js
+++ eg/TheArduinoStarterKit/ColorMixingLamp.js
@@ -3,16 +3,13 @@ var Edison = require("edison-io");
 var board = new five.Board({io: new Edison()});
 
 board.on("ready", function() {
-
   var led = new five.Led.RGB([6,5,3]);
-  var b = new Buffer(3);
   var sensors = new five.Sensors([{pin:"A0"},{pin:"A1"},{pin:"A2"}]);
   sensors.on("change",function() {
-    b[0] = sensors[0].analog;
-    b[1] = sensors[1].analog;
-    b[2] = sensors[2].analog;
-    var c = b.toString("hex");
+    var c = sensors.reduce(function(s, sensor) {
+      return s + sensor.analog.toString(16);
+    }, "#");
     console.log(c);
-    led.color("#" + c);
+    led.color(c);
   });
 });
--- eg/TheArduinoStarterKit/DigitalHourglass.js
+++ eg/TheArduinoStarterKit/DigitalHourglass.js
@@ -4,7 +4,7 @@ var board = new five.Board({io: new Edison()});
 
 board.on("ready", function() {
   var leds = new five.Leds([2,3,4,5,6,7]);
-  var tilt = new five.Sensor({pin:8,type:"digital"});
+  var tilt = new five.Sensor.Digital(8);
   var i = 0;
   this.loop(1000*60*10,function() {
     leds[i].on();
