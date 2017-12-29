b44a230316b51c4e696b6acafd8503d5249ca933
dnlup <dwon.dnl@gmail.com>
2017-12-29 12:43:12 +0100

    Altimeter: add BMP180 fractional digits
    Fractional digits are already 2, add test.

--- test/altimeter.js
+++ test/altimeter.js
@@ -456,7 +456,7 @@ exports["Altimeter - BMP180"] = {
   },
 
   resolution: function(test) {
-    test.expect(4);
+    test.expect(6);
 
     var driver = IMU.Drivers.get(this.board, "BMP180");
     var dataSpy = this.sandbox.spy();
@@ -475,6 +475,9 @@ exports["Altimeter - BMP180"] = {
 
     test.equal(this.altimeter.meters, 10.12);
     test.equal(this.altimeter.feet, 33.2);
+    test.equal(digits.fractional(this.altimeter.meters), 2);
+    test.equal(digits.fractional(this.altimeter.feet), 1);
+
     test.done();
   },
 };
