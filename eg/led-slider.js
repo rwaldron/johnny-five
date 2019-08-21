7e0b72ea4efa215cd8f9530422a24259bf1360dc
Ryan Gill <ryanjgill2@gmail.com>
2019-08-21 13:06:56 -0500

    use scaleTo

--- eg/led-slider.js
+++ eg/led-slider.js
@@ -1,12 +1,12 @@
 const {Board, Led, Sensor} = require("../lib/johnny-five.js");
 const board = new Board();
 
-board.on("ready", function() {
+board.on("ready", () => {
   const slider = new Sensor("A0");
   const led = new Led(11);
 
   // Scale the sensor's value to the LED's brightness range
-  slider.scale([0, 255]).on("data", () => {
-    led.brightness(slider.value);
+  slider.on("data", () => {
+    led.brightness(slider.scaleTo([0, 255]));
   });
 });
