bc4933e7f0b1ea8ead257aad4a0cbc5fa29bc34e
Rick Waldron <waldron.rick@gmail.com>
2019-09-10 11:07:52 -0400

    Examples: make Barometer example output consistent

--- eg/barometer-BMP180.js
+++ eg/barometer-BMP180.js
@@ -1,14 +1,14 @@
-var five = require("../");
-var board = new five.Board();
+const {Barometer, Board} = require("../lib/johnny-five.js");
+const board = new Board();
 
-board.on("ready", function() {
-  var barometer = new five.Barometer({
+board.on("ready", () => {
+  const barometer = new Barometer({
     controller: "BMP180"
   });
 
-  barometer.on("change", function() {
-    console.log("Barometer");
-    console.log("  pressure     : ", this.pressure);
+  barometer.on("change", () => {
+    console.log("Barometer:");
+    console.log("  pressure     : ", barometer.pressure);
     console.log("--------------------------------------");
   });
 });
--- eg/barometer-MS5611.js
+++ eg/barometer-MS5611.js
@@ -1,14 +1,14 @@
-var five = require("../");
-var board = new five.Board();
+const {Barometer, Board} = require("../lib/johnny-five.js");
+const board = new Board();
 
-board.on("ready", function() {
-  var pressure = new five.Barometer({
-    controller: "MS5611",
+board.on("ready", () => {
+  const barometer = new Barometer({
+    controller: "MS5611"
   });
 
-  pressure.on("change", function() {
-    console.log("Barometer");
-    console.log("  pressure     : ", this.pressure);
+  barometer.on("change", () => {
+    console.log("Barometer:");
+    console.log("  pressure     : ", barometer.pressure);
     console.log("--------------------------------------");
   });
 });
