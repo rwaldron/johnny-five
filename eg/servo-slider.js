8567980edb024e8590095dc05c9d479118b99b8b
Katie Broida <kbroida@gmail.com>
2020-01-10 15:53:45 -0500

    Switch to 'change' event

--- eg/servo-slider.js
+++ eg/servo-slider.js
@@ -6,7 +6,7 @@ board.on("ready", () => {
   const slider = new Sensor("A0");
   const tilt = new Servo(10);
 
-  slider.on("slide", () => {
+  slider.on("change", () => {
     tilt.to(slider.scaleTo(0, 180));
   });
 });
